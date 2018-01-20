1b0f36b2be0a6d884dd999df4a67cd770bb882f5
nir <gib.nir@gmail.com>
2018-01-20 22:37:07 +0200

    update files.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 
 /*
 FilterableProductTable (orange): contains the entirety of the example
